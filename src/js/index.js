534df743b9258992c3fd0569b261a906952b95a7
Camila <camilasanchezvinik@hotmail.com>
2021-08-30 21:37:24 -0300

    exercise

--- src/js/index.js
+++ src/js/index.js
@@ -31,19 +31,31 @@ function render(variables = {}) {
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+        ${cover}
+      <img src="${variables.avatarURL}" class="photo" />
+      <h1>${variables.name ? variables.name : ""} ${
+    variables.lastname ? variables.lastname : ""
+  }</h1>
+      <h2>${variables.role ? variables.role : ""}</h2>
+      <h3>${variables.city ? variables.city : ""}, ${
+    variables.country ? variables.country : ""
+  }</h3>
+      <ul class="${variables.socialMediaPosition}">
+        <li><a href={variables.twitter?"https://twitter.com/${
+          variables.twitter ? variables.twitter : ""
+        }"}><i class="fa fa-twitter"></i></a></li>
+        <li> <a href="https://github.com/${
+          variables.github ? variables.github : ""
+        }"><i class="fa fa-github"></i></a></li >
+        <li><a href="https://linkedin.com/${
+          variables.linkedin ? variables.linkedin : ""
+        }"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="https://instagram.com/${
+          variables.instagram ? variables.instagram : ""
+        }"><i class="fa fa-instagram"></i></a></li>
+      </ul >
+    </div >
+`;
 }
 
 /**
@@ -54,9 +66,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://media-exp1.licdn.com/dms/image/C4E16AQECp4mF9aWLIw/profile-displaybackgroundimage-shrink_200_800/0/1629498848719?e=1635984000&v=beta&t=oYGxU7WLmgYFiukE2rPXbIgh-ZmiUy8-jI2xRCdgy4I",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://avatars.githubusercontent.com/u/73668147?v=4",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
@@ -73,7 +86,8 @@ window.onload = function() {
   render(window.variables); //render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
-    elm.addEventListener("change", function(e) {
+    let evt = elm.type === "text" ? "keyup" : "change";
+    elm.addEventListener(evt, function(e) {
       // <- add a listener to every input
       const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
       let values = {};
